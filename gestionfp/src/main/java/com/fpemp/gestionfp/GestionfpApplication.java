be9733f7adc42038935ab05cc42b3003ced6135d
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2026-05-18 19:29:51 +0200

    Crear estructura basica del proyecto con spring initilz

---
+++ gestionfp/src/main/java/com/fpemp/gestionfp/GestionfpApplication.java
@@ -0,0 +1,13 @@
+package com.fpemp.gestionfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionfpApplication.class, args);
+	}
+
+}
